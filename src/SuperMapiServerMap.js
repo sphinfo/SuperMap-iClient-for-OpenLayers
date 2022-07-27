c92060893da238f345d1ae3cd2cf8f0653fc68d7
hjlee <hjlee@sphinfo.co.kr>
2022-07-27 18:44:16 +0900

    SuperMap iServer 발행 맵 호출

---
+++ src/SuperMapiServerMap.js
@@ -0,0 +1,25 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import { TileSuperMapRest } from '@supermap/iclient-ol';
+
+var url = "https://iserver.supermap.io/iserver/services/map-world/rest/maps/World";
+var map = new Map({
+  target: 'map',
+  view: new View({
+      center: [0, 0],
+      zoom: 2,
+      projection: 'EPSG:4326'
+  })
+});
+
+var layer = new TileLayer({
+      source: new TileSuperMapRest({
+          url: url,
+          wrapX: true,
+          projection: "EPSG:4326"
+      })
+});
+
+map.addLayer(layer);
