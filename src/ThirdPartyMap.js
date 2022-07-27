071e794c31bbd3faed8637e455b64da45f4404c1
hjlee <hjlee@sphinfo.co.kr>
2022-07-27 18:45:04 +0900

    Third-party 맵 호출

---
+++ src/ThirdPartyMap.js
@@ -0,0 +1,26 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import OSM from 'ol/source/OSM';
+import TileLayer from 'ol/layer/Tile';
+import {FullScreen, defaults as defaultControls} from 'ol/control';
+
+var map = new Map({
+  controls: defaultControls().extend([
+    new FullScreen({
+        source: 'fullscreen'
+    })
+  ]),
+  target: 'map',
+  view: new View({
+      center: [0, 0],
+      zoom: 14,
+      projection: 'EPSG:4326'
+  })
+});
+
+var layer = new TileLayer({
+      source: new OSM()
+});
+
+map.addLayer(layer);
