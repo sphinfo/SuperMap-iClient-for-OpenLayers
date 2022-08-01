eced0d97f767d73f68b21f77b06906dbc2e85cf5
hjlee <hjlee@sphinfo.co.kr>
2022-08-01 17:49:20 +0900

    맵 좌표계 설정(주석추가)

--- src/Set-projection-for-the-map.js
+++ src/Set-projection-for-the-map.js
@@ -5,13 +5,31 @@ import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
 import Projection from 'ol/proj/Projection';
 
-var custProj = new Projection({
-    code: 'EPSG:4326'
+//1) View 속성의 projection 설정
+// var map = new Map({
+//     view: new View({
+//         projection: 'EPSG:4326',
+//         center: [0, 0],
+//         zoom: 2        
+//     }),
+//     layers: [
+//         new TileLayer({
+//         source: new OSM()
+//         }),
+//     ],
+//     target: 'map'
+// });
+
+//2) ol.proj.Projection 클래스를 통해 사용자 정의 투영 매개변수 설정
+var custProjection = new Projection({
+    code: 'EPSG:4326',
+    extent: [-180.0, -90.0, 180.0, 90.0], //Projected bounds (https://epsg.io/ 참조)
+    units: 'm'
 });
 
 var map = new Map({
     view: new View({
-        projection: custProj.projection,
+        projection: custProjection,
         center: [0, 0],
         zoom: 2        
     }),
