e5559cde8bc7b4f6921c771ced2fd1b1019591a6
hjlee <hjlee@sphinfo.co.kr>
2022-08-02 16:41:35 +0900

    Zoom 컨트롤 추가(ZoomSlider추가)

--- src/Add-controls-Zoom.js
+++ src/Add-controls-Zoom.js
@@ -3,15 +3,17 @@ import Map from 'ol/Map';
 import OSM from 'ol/source/OSM';
 import TileLayer from 'ol/layer/Tile';
 import View from 'ol/View';
-import {Zoom, defaults as defaultControls} from 'ol/control';
+import {Zoom, ZoomSlider, defaults as defaultControls} from 'ol/control';
 
 var zoomControl = new Zoom({
-    zoomInLabel: '확대',
-    zoomOutLabel : '축소'
+    // zoomInLabel: '확대', //확대버튼라벨
+    // zoomOutLabel : '축소', //축소버튼라벨
+    zoomInTipLabel: '확대', //확대버튼 팁라벨
+    zoomOutTipLabel: '축소' //축소버튼 팁라벨
 });
 
 var map = new Map({
-    controls: defaultControls().extend([zoomControl]),
+    controls: defaultControls().extend([zoomControl,new ZoomSlider()]),
     view: new View({
         projection: 'EPSG:4326',
         center: [127.1698, 37.7617],
