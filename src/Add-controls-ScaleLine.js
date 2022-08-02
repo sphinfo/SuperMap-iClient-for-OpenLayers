02e0a48c97841ac954d8b97a81e1227eafe74ffa
hjlee <hjlee@sphinfo.co.kr>
2022-08-02 17:17:00 +0900

    ScaleLine 컨트롤 추가(bar형태 추가)

--- src/Add-controls-ScaleLine.js
+++ src/Add-controls-ScaleLine.js
@@ -6,7 +6,11 @@ import View from 'ol/View';
 import {ScaleLine,  defaults as defaultControls} from 'ol/control';
 
 var scaleLineControl = new ScaleLine({
-    units: 'us'
+    units: 'metric', //스케일라인 단위 'degrees', 'imperial', 'nautical', 'metric'(기본값), 'us'
+    bar: true, //선 대신 스케일바 렌더링
+    text: true, //스케일바 위에 텍스트 렌더링
+    steps: 6, //스케일바 단계설정, 기본값 4 (짝수로 설정권장)
+    minWidth: 125 //OGC 기본 dpi의 최소 너비(픽셀), 기본값 64
 });
 
 var map = new Map({
