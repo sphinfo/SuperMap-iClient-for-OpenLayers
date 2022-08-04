0ef569b94d86e965ce41c5342afe572caa296ae4
hjlee <hjlee@sphinfo.co.kr>
2022-08-04 14:29:49 +0900

    지도정보검색(SQL문WHERE절)

---
+++ src/mapQueryBySpecifyingConditions.js
@@ -0,0 +1,59 @@
+import 'ol/ol.css';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+import { TileSuperMapRest, GetFeaturesBySQLParameters, FeatureService } from '@supermap/iclient-ol';
+
+const baseUrl = "https://iserver.supermap.io/iserver/services/map-world/rest/maps/World";
+const dataUrl = "https://iserver.supermap.io/iserver/services/data-world/rest/data";
+
+const map = new Map({
+  target: 'map', //지도 컨테이너 ID
+  view: new View({
+      center: [127.77, 35.89], //지도의 중심점
+      zoom: 5, //지도의 초기 해상도를 계산하는 확대/축소 레벨
+      projection: 'EPSG:4326' //지도 좌표계
+  })
+});
+
+const layer = new TileLayer({
+      source: new TileSuperMapRest({
+          url: baseUrl, //SuperMap iServer에 발행된 맵서비스 주소
+          wrapX: true //타일을 반경선 너머로 렌더링
+      }),
+      projection: 'EPSG:4326' //레이어 좌표계
+});
+
+map.addLayer(layer);
+
+query();
+
+function query(){
+
+    //조회대상
+    var param = new GetFeaturesBySQLParameters({
+        queryParameter: {
+            name: "Countries@World", //Dataset Name or Layer Name
+            attributeFilter: "SMID = 234" //필터 조건, SQL문의 WHERE절과 동일
+        },
+        datasetNames: ["World:Countries"] //[Datasource Name:Dataset Name]
+    });
+
+    new FeatureService(dataUrl).getFeaturesBySQL(param, function (serviceResult) {
+        //조회결과
+        var vectorSource = new VectorSource({
+            features: (new GeoJSON()).readFeatures(serviceResult.result.features),
+            wrapX: false
+        });
+
+        //조회결과 레이어
+        var resultLayer = new VectorLayer({
+            source: vectorSource
+        });
+
+        map.addLayer(resultLayer);
+    });
+}
